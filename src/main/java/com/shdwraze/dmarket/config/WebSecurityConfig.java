784ee292cae75c6d37559f0bcb3259fa985607e8
shdwraze <dedmoped1997@gmail.com>
2022-09-30 14:20:32 +0300

    Add authorization and WebSecurityConfig

---
+++ src/main/java/com/shdwraze/dmarket/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.shdwraze.dmarket.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .loginProcessingUrl("/login")
+                    .defaultSuccessUrl("/", true)
+                    .permitAll()
+                .and()
+                    .logout()
+                    .logoutSuccessUrl("/")
+                    .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .passwordEncoder(encoder())
+                .usersByUsernameQuery("select login, password, 'true' as enabled from accounts where login=?")
+                .authoritiesByUsernameQuery("select login, role from accounts where login=?");
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/shdwraze/dmarket/controller/MainController.java
+++ src/main/java/com/shdwraze/dmarket/controller/MainController.java
@@ -1,19 +1,14 @@
 package com.shdwraze.dmarket.controller;
 
-import com.shdwraze.dmarket.entity.Account;
 import com.shdwraze.dmarket.repo.AccountRepository;
 import com.shdwraze.dmarket.repo.HeroRepository;
 import com.shdwraze.dmarket.repo.ItemRepository;
 import com.shdwraze.dmarket.repo.LotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@RestController
+@Controller
 public class MainController {
     @Autowired
     private AccountRepository accountRepository;
@@ -24,8 +19,8 @@ public class MainController {
     @Autowired
     private LotRepository lotRepository;
 
-    @GetMapping({"/", ""})
-    public List<Account> index() {
-        return accountRepository.findAll();
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
