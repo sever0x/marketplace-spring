6d6e00774e09bc9484a2d8fa018dae1324d5a597
shdwraze <dedmoped1997@gmail.com>
2022-10-08 15:48:33 +0300

    Added the ability to delete a lot

--- src/main/java/com/shdwraze/dmarket/controller/LotController.java
+++ src/main/java/com/shdwraze/dmarket/controller/LotController.java
@@ -13,10 +13,7 @@ import com.shdwraze.dmarket.repo.LotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
@@ -72,5 +69,12 @@ public class LotController {
         lotRepository.save(lot);
 
         return "redirect:/";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteLot(@PathVariable int id) {
+        lotRepository.deleteById(id);
+
+        return "redirect:/lots";
     }
 }
